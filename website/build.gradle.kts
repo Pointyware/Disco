0af36488f3a9a6c2c2a55a3303e4cb989992ac69
Taush Sampley <taushsampley@gmail.com>
2025-07-30 20:24:22 -0500

    Add kotlinx-datetime and -html

---
+++ website/build.gradle.kts
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2024-2025 Pointyware. Use of this software is governed by the Affero GPL-3.0 license.
+ */
+
+plugins {
+    alias(libs.plugins.kotlinJvm)
+    application
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+dependencies {
+
+    implementation(libs.kotlinx.dateTime)
+    implementation(libs.kotlinx.coroutinesCore)
+    implementation(libs.koin.core)
+
+    implementation(libs.kotlinx.html.jvm)
+
+    testImplementation(libs.kotlin.test)
+}
+
+application {
+    mainClass = "org.pointyware.disco.site.MainKt"
+}
