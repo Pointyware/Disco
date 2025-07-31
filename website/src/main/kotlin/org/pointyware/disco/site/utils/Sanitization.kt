88308872fff0b1f5b0530969dfa3c21b959427d2
Taush Sampley <taushsampley@gmail.com>
2025-07-30 20:23:34 -0500

    Copy site builder logic from XYZ

---
+++ website/src/main/kotlin/org/pointyware/disco/site/utils/Sanitization.kt
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2025 Pointyware. Use of this software is governed by the Affero GPL-3.0 license.
+ */
+
+package org.pointyware.xyz.site.utils
+
+/**
+ * Transforms the input to conform with our rules for HTML pages.
+ *
+ * 1. An html file must end with .html
+ * 2. All whitespaces are replaced with underscores
+ */
+fun sanitizeHtmlFileName(input: String): String =
+    input.replace("\\s".toRegex(), "_").let {
+        if (it.endsWith(".html")) {
+            it
+        } else {
+            "$it.html"
+        }
+    }
