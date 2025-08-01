2da22ad60be0de953254ef05795a8e6858837b73
Taush Sampley <taushsampley@gmail.com>
2025-08-01 15:37:29 -0500

    Add computation graph class

---
+++ core-entities/src/commonMain/kotlin/org/pointyware/disco/entities/math/ComputationGraph.kt
@@ -0,0 +1,40 @@
+package org.pointyware.disco.entities.math
+
+/**
+ * A computation graph consists of nodes that represent operations and edges that
+ * represent dependencies between operations in the graph.
+ *
+ * A computation graph works on a ContextMap with values identified by typed keys.
+ * The computation retrieves the identified values in a type-safe manner to perform
+ * its operations, before inserting the results back into the map.
+ */
+class ComputationGraph(
+    val nodes: Set<Node<*>>,
+    val edges: Set<Edge<*, *>>
+) {
+
+    /**
+     * A computation node represents an operation in the computation graph.
+     */
+    abstract class Node<O: Any>(
+        val inputs: Set<ComputationKey<*>>,
+        val output: ComputationKey<O>
+    ) {
+        /**
+         * Computes the output value of the node in terms of its inputs.
+         */
+        abstract fun compute(): O
+
+        /**
+         * Computes the derivative of the output value of the node in terms of its inputs.
+         */
+        abstract fun derivative(): O
+    }
+    /**
+     * A computation edge represents a dependency between two computation nodes in the graph.
+     */
+    data class Edge<I: Any, O: Any>(
+        val from: Node<I>,
+        val to: Node<O>
+    )
+}
