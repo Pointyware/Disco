f26563696df45b233befefc3d4cb03f93e672605
Taush Sampley <taushsampley@gmail.com>
2025-08-04 13:39:38 -0500

    Add SumLayer to layer computationcomponent hierarchy

--- core-entities/src/commonMain/kotlin/org/pointyware/disco/entities/layers/SumLayer.kt
+++ core-entities/src/commonMain/kotlin/org/pointyware/disco/entities/layers/SumLayer.kt
@@ -4,11 +4,42 @@
 
 package org.pointyware.disco.entities.layers
 
+import org.pointyware.disco.entities.ExperimentalNetworkApi
+import org.pointyware.disco.entities.math.ComputationComponent
+import org.pointyware.disco.entities.math.ComputationContext
+import org.pointyware.disco.entities.math.ComputationGraph
+import org.pointyware.disco.entities.math.ComputationKey
 import org.pointyware.disco.entities.tensors.Tensor
 
 /**
  *
  * TODO: implement Layer interface after graph calculations
  */
-class SumLayer {
+class SumLayer(
+    val inputIds: Set<ComputationKey<Tensor>>,
+    val outputId: ComputationKey<Tensor>,
+): ComputationComponent {
+    @ExperimentalNetworkApi
+    override val graph: ComputationGraph
+        get() = ComputationGraph(
+            nodes = emptySet(),
+            edges = emptySet()
+        )
+
+    @OptIn(ExperimentalNetworkApi::class)
+    class SumNode(
+        val inputIds: Set<ComputationKey<Tensor>>,
+        val outputId: ComputationKey<Tensor>,
+    ): ComputationGraph.Node() {
+        override fun compute(context: ComputationContext) {
+            val output = context.get(outputId)
+            inputIds.forEach {
+                output += context.get(it)
+            }
+        }
+
+        override fun derivative(context: ComputationContext) {
+            TODO("Not yet implemented")
+        }
+    }
 }
